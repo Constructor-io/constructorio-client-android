c8410506158d776dbfb17ec283aea41f04430a57
qbasso <jakub.porzuczek@gmail.com>
2018-06-20 10:55:18 +0200

    add button to manually trigger search results loaded event

--- sample/src/main/java/io/constructor/sample/MainActivity.kt
+++ sample/src/main/java/io/constructor/sample/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import io.constructor.core.ConstructorIo
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +16,6 @@ class MainActivity : AppCompatActivity() {
         button2.setOnClickListener { startActivity(Intent(this, SampleActivityCustom::class.java)) }
         button3.setOnClickListener { ConstructorIo.triggerConversionEvent("testId", "$11.99") }
         button4.setOnClickListener { ConstructorIo.triggerSearchResultClickThroughEvent("testTerm", "testId", "1") }
+        button5.setOnClickListener { ConstructorIo.triggerSearchResultLoadedEvent("testTerm", Random().nextInt(99) + 1) }
     }
 }
