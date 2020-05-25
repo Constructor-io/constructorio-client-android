fb22033eca853d522b5ea2a04cb9c5465ceca3ea
Zubin Tiku <rubikzube@gmail.com>
2020-05-25 12:56:05 -0400

    More fixes

--- library/src/main/java/io/constructor/injection/module/NetworkModule.kt
+++ library/src/main/java/io/constructor/injection/module/NetworkModule.kt
@@ -1,15 +1,14 @@
 package io.constructor.injection.module
 
 import android.content.Context
+import com.squareup.moshi.KotlinJsonAdapterFactory
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import io.constructor.BuildConfig
-import io.constructor.data.interceptor.TokenInterceptor
+import io.constructor.data.interceptor.RequestInterceptor
 import io.constructor.data.local.PreferencesHelper
 import io.constructor.data.memory.ConfigMemoryHolder
-import io.constructor.data.model.dataadapter.ResultDataAdapter
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -34,11 +33,11 @@ class NetworkModule(private val context: Context) {
     @Provides
     @Singleton
     internal fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor,
-                                     tokenInterceptor: TokenInterceptor): OkHttpClient {
+                                     requestInterceptor: RequestInterceptor): OkHttpClient {
         val httpClientBuilder = OkHttpClient.Builder()
-        httpClientBuilder.addInterceptor(tokenInterceptor)
+        httpClientBuilder.addInterceptor(requestInterceptor)
         if (BuildConfig.DEBUG) {
-            httpClientBuilder.addInterceptor(httpLoggingInterceptor)
+            // httpClientBuilder.addInterceptor(httpLoggingInterceptor)
         }
         return httpClientBuilder.build()
 
@@ -51,13 +50,12 @@ class NetworkModule(private val context: Context) {
 
     @Provides
     @Singleton
-    internal fun provideTokenInterceptor(prefHelper: PreferencesHelper, configMemoryHolder: ConfigMemoryHolder): TokenInterceptor = TokenInterceptor(context, prefHelper, configMemoryHolder)
+    internal fun provideRequestInterceptor(prefHelper: PreferencesHelper, configMemoryHolder: ConfigMemoryHolder): RequestInterceptor = RequestInterceptor(context, prefHelper, configMemoryHolder)
 
     @Provides
     @Singleton
     internal fun provideMoshi(): Moshi = Moshi
             .Builder()
-            .add(ResultDataAdapter())
             .add(KotlinJsonAdapterFactory())
             .build()
 }
