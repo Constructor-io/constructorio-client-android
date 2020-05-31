3f15d38f3b68d531b43e62edf8bc4e8994df421a
Zubin Tiku <rubikzube@gmail.com>
2020-05-31 12:06:59 -0400

    Fixed the bodega code

--- sample/src/main/java/io/constructor/sample/feature/checkout/CheckoutPresenter.kt
+++ sample/src/main/java/io/constructor/sample/feature/checkout/CheckoutPresenter.kt
@@ -4,6 +4,7 @@ import io.constructor.core.ConstructorIo
 import io.constructor.sample.common.BasePresenter
 import io.constructor.sample.data.CartDataStorage
 import io.constructor.sample.extensions.price
+import kotlin.random.Random
 
 class CheckoutPresenter(view: CheckoutView, private val cartStorage: CartDataStorage) : BasePresenter<CheckoutView>(view) {
 
@@ -11,7 +12,7 @@ class CheckoutPresenter(view: CheckoutView, private val cartStorage: CartDataSto
         val total = cartStorage.getCartContent().map { it.value }.sumByDouble {
             ((it.first.result.price() ?: 0.0) * it.second)
         }
-        ConstructorIo.trackPurchase(arrayOf(ConstructorIo.getClientId()), total)
+        ConstructorIo.trackPurchase(arrayOf(ConstructorIo.getClientId()), total, "ORD" + Random.nextInt(0, 10000))
     }
 
 }
