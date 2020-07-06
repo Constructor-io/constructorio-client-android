bdbcb9bf02997d2148040aa81af0af09e3cb06b5
Zubin Tiku <rubikzube@gmail.com>
2020-07-06 10:30:40 -0400

    Fixes

--- library/src/test/java/io/constructor/test/helpers.kt
+++ library/src/test/java/io/constructor/test/helpers.kt
@@ -6,6 +6,7 @@ import io.constructor.data.local.PreferencesHelper
 import io.constructor.data.memory.ConfigMemoryHolder
 import io.constructor.data.remote.ConstructorApi
 import io.constructor.injection.module.NetworkModule
+import java.util.concurrent.TimeUnit
 
 /**
  * Creates a data manager that communicates with a Mock Web Server
@@ -18,7 +19,10 @@ fun createTestDataManager(preferencesHelper: PreferencesHelper,
     val loggingInterceptor = networkModule.provideHttpLoggingInterceptor()
     val requestInterceptor = networkModule.provideRequestInterceptor(preferencesHelper, configMemoryHolder)
     val moshi = networkModule.provideMoshi()
-    val okHttpClient = networkModule.provideOkHttpClient(loggingInterceptor, requestInterceptor)
+    val okHttpClient = networkModule.provideOkHttpClient(loggingInterceptor, requestInterceptor).newBuilder().addInterceptor { chain ->
+        var request = chain.request()
+        chain.proceed(request)
+    }.readTimeout(1, TimeUnit.SECONDS).build()
     val retrofit = networkModule.provideRetrofit(okHttpClient, moshi, preferencesHelper)
     val constructorApi = retrofit.create(ConstructorApi::class.java)
     return DataManager(constructorApi, moshi);
