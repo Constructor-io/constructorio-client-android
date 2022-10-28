d5c0e6221b9b995614d66bc7929cce91123f7a55
Jimmy Li <jimmyli921@gmail.com>
2022-10-28 15:59:48 -0700

    Add tests around Request builder

--- library/src/test/java/io/constructor/data/builder/QuizRequestTest.kt
+++ library/src/test/java/io/constructor/data/builder/QuizRequestTest.kt
@@ -1,4 +1,53 @@
 package io.constructor.data.builder
 
+import org.junit.Test
+import kotlin.test.assertEquals
+
 class QuizRequestTest {
+    private val quizId = "test-quiz"
+    private val versionId = "11db5ac7-67e1-4000-9000-414d8425cab3"
+    private val section = "Products"
+    private val a = listOf("1", "1,2")
+
+    @Test
+    fun quizRequestUsingBuilder() {
+        val request = QuizRequest.Builder(quizId).build()
+        assertEquals(request.quizId, quizId)
+    }
+
+    @Test
+    fun quizRequestWithVersionIdUsingBuilder() {
+        val request = QuizRequest.Builder(quizId)
+            .setVersionId(versionId)
+            .build()
+        assertEquals(request.versionId, versionId)
+    }
+
+    @Test
+    fun quizRequestWithAnswersUsingBuilder() {
+        val request = QuizRequest.Builder(quizId)
+            .setA(a)
+            .build()
+        assertEquals(request.a, a)
+    }
+
+    @Test
+    fun quizRequestWithSectionUsingBuilder() {
+        val request = QuizRequest.Builder(quizId)
+            .setSection(section)
+            .build()
+        assertEquals(request.section, section)
+    }
+
+    @Test
+    fun quizRequestUsingDSL() {
+        val request = QuizRequest.build(quizId) {
+            versionId = this@QuizRequestTest.versionId
+            a = this@QuizRequestTest.a
+            section = this@QuizRequestTest.section
+        }
+        assertEquals(request.versionId, versionId)
+        assertEquals(request.a, a)
+        assertEquals(request.section, section)
+    }
 }
